de32ef6247a64ab516a6d4c392736baca7f3331b
bintangglenn <bintang.glenn@ui.ac.id>
2016-12-27 11:37:00 -1200

    Iniial Commit

---
+++ app.php
@@ -0,0 +1,51 @@
+<?php 
+if (! $_POST) {echo "400 Bad Request"; die();} session_start();
+if(isset($_FILES['document'])){
+	
+	$file_name = $_FILES['document']['name'];
+	
+	$file_size = $_FILES['document']['size'];
+	if($file_size > 1000000) {
+		$file_size = round($file_size / 1048576, 2) . " MB";
+	}
+	else if($file_size > 1024) {
+		$file_size = round($file_size / 1024, 2) . " KB";
+	}
+	else {
+		$file_size .= " B";
+	}
+	
+	$file_tmp = $_FILES['document']['tmp_name'];
+ 	
+ 	$file_type = $_FILES['document']['type'];
+ 	
+ 	$file_ext = pathinfo($file_name,PATHINFO_EXTENSION);
+
+ 	$time = date_create(null, timezone_open("Asia/Jakarta"));
+ 	$uploadTime = date_format($time, "d-m-Y H:m:s");
+ 	
+ 	if(($file_ext == "pdf" || $file_ext == "docx") && $file_size <= 4194304) {
+ 		move_uploaded_file($file_tmp, ("./uploads/".$file_name));
+ 		$_SESSION['valid'] = 'File berhasil diupload';
+ 		array_push($_SESSION['dataUpload'], "<tr><td>" . $file_name . "</td><td>" . $file_size . "</td><td>" . $uploadTime . "</td><td><form action=\"app.php\" method=\"post\" enctype=\"multipart/form-data\"><input type=\"hidden\" value=\"" . $file_name . "\" name=\"hapus\"/><input type=\"hidden\" value=\"" . $_SESSION['idx'] . "\" name=\"idxHapus\"/><input type=\"Submit\" value=\"Hapus\" name=\"submit\"/></form></td><td><form action=\"app.php\" method=\"post\" enctype=\"multipart/form-data\"><input type=\"hidden\" value=\"" . $file_name . "\" name=\"download\"/><input type=\"submit\" value=\"Download\" name=\"submit\"/></form></td>");
+ 		$_SESSION['idx'] += 1;
+ 	}
+ 	else {
+ 		$_SESSION['valid'] = 'File gagal divalidasi, tidak bisa diupload';
+ 	}
+ 	header("location: index.php");
+}
+
+if(isset($_POST['download'])) {
+	header("Content-Type: application/pdf");
+	header("Content-Transfer-Encoding: Binary");
+	header("content-disposition: attachment; filename=\"" . $_POST['download'] . "\"");
+	readfile("/uploads/" . $_POST['donwload']);
+	header("location: index.php");
+}
+
+if(isset($_POST['hapus'])) {
+	unlink("./uploads/" . $_POST['hapus']);
+	unset($_SESSION['dataUpload'][$_POST['idxHapus']]);
+	header("location: index.php");
+}
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Upload | Tutorial 7</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="libs/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container" style="margin-top: 3vh;">
+        <div class="col-md-5">
+            <form class="form-horizontal" action="app.php" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="document">File:</label>
+                    <div class="col-sm-10">
+                      <input type="file" name="document" id="document" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <input type="submit" value="Upload" name="submit" class="btn btn-default">
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <?php
+            if(isset($_SESSION['valid'])) {
+                echo $_SESSION['valid'];
+                unset($_SESSION['valid']);
+            }
+            if(!isset($_SESSION['dataUpload'])) {
+                $_SESSION['dataUpload'] = array();
+            }
+            if(!isset($_SESSION['idx'])) {
+                 $_SESSION['idx'] = 0;
+            }           
+        ?>
+        <div class="result">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Nama File</th>
+                        <th>Ukuran File</th>
+                        <th>Waktu Upload</th>
+                        <th>Hapus</th>
+                        <th>Download</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        if(isset($_SESSION['dataUpload'])) {
+                            for($i = 0; $i < count($_SESSION['dataUpload']); $i++) {
+                                echo $_SESSION['dataUpload'][$i];
+                            }  
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <script src="libs/jquery/dist/jquery.min.js"></script>
+    <script src="libs/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
