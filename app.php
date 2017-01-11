090da054531284114b8b4dfb83d66fbf3487f19c
bintangglenn <bintang.glenn@ui.ac.id>
2017-01-10 16:03:24 -1200

    Fix layout and delete unwanted detail

--- app.php
+++ app.php
@@ -4,27 +4,12 @@ if(isset($_FILES['file']) && isset($_FILES['cert'])) {
 	$file_name = $_FILES['file']['name'];
 	$cert_name = $_FILES['cert']['name'];
 	
-	$file_size = $_FILES['file']['size'];
-	if($file_size > 1000000) {
-		$file_size = round($file_size / 1048576, 2) . " MB";
-	}
-	else if($file_size > 1024) {
-		$file_size = round($file_size / 1024, 2) . " KB";
-	}
-	else {
-		$file_size .= " B";
-	}
-	
 	$file_tmp = $_FILES['file']['tmp_name'];
 	$cert_tmp = $_FILES['cert']['tmp_name'];
  	
  	$file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
  	$cert_ext = pathinfo($cert_name, PATHINFO_EXTENSION);
 
- 	$time = date_create(null, timezone_open("Asia/Jakarta"));
- 	$uploadTime = date_format($time, "d-m-Y H:m:s");
-
- 	// Check if already signed
  	$multiple = false;
  	foreach ($_SESSION['dataUpload'] as $value) {
  		if(strpos($value, $file_name) !== false) {
@@ -36,29 +21,37 @@ if(isset($_FILES['file']) && isset($_FILES['cert'])) {
  	move_uploaded_file($cert_tmp, ("./tmp/" . $cert_name));
  	move_uploaded_file($file_tmp, ("./tmp/" . $file_name));
 
-	if($cert_ext == "p12") {
- 		exec('openssl pkcs12 -in ' . '"./tmp/' . $cert_name . '" -out ' . '"./tmp/' . substr($cert_name, 0, -4) . '.pem" -passin pass:' . $_POST['pass'] . ' -passout pass:' . $_POST['pass']);
- 		unlink("./tmp/" . $cert_name);
- 		$cert_name = substr($cert_name, 0, -4) . ".pem";
- 	}
-
- 	if($_POST['submit'] == "sign" && !$multiple) {
- 		$out = shell_exec('openssl dgst -sha256 -sign "./tmp/' . $cert_name . '" -out "./signature/' . $file_name . '.sha256" -passin pass:' . $_POST['pass'] . ' "./tmp/' . $file_name . '" 2>&1');
- 		var_dump($out);
- 		if(empty($out)) {
-	 		$tmp = "key" . $_SESSION['idx'];
-	 		$_SESSION['dataUpload'][$tmp] = "<tr><td>" . $file_name . "</td><td>" . $uploadTime . "</td><td><form action=\"app.php\" method=\"post\" enctype=\"multipart/form-data\"><input type=\"hidden\" value=\"" . $file_name . ".sha256\" name=\"hapus\"/><input type=\"hidden\" value=\"" . $_SESSION['idx'] . "\" name=\"idxHapus\"/><input type=\"Submit\" value=\"Delete\" name=\"submit\"/></form></td><td>" . $file_size . "</td>";
-	 		$_SESSION['idx'] += 1;
-	 		$_SESSION['valid'] = 'Sign Success!';
-	 	} else {
-	 		$_SESSION['valid'] = 'There is an error while signing';
+ 	if($cert_ext == "p12" || $cert_ext == "pem") {
+		if($cert_ext == "p12") {
+	 		exec('openssl pkcs12 -in ' . '"./tmp/' . $cert_name . '" -out ' . '"./tmp/' . substr($cert_name, 0, -4) . '.pem" -passin pass:' . $_POST['pass'] . ' -passout pass:' . $_POST['pass']);
+	 		unlink("./tmp/" . $cert_name);
+	 		$cert_name = substr($cert_name, 0, -4) . ".pem";
 	 	}
- 	}
- 	else if($_POST['submit'] == "verify") {
- 		exec('openssl x509 -in "./tmp/' . $cert_name . '" -pubkey -noout > "./tmp/' . $file_name . '.pub"');
- 		$out = shell_exec('openssl dgst -sha256 -verify "./tmp/' . $file_name . '.pub" -signature "./signature/' . $file_name . '.sha256" "./tmp/' . $file_name . '"');
- 		$_SESSION['valid'] = $out;
- 	}
+
+	 	if($_POST['submit'] == "sign" && !$multiple) {
+	 		$out = shell_exec('openssl dgst -sha256 -sign "./tmp/' . $cert_name . '" -out "./signature/' . $file_name . '.sha256" -passin pass:' . $_POST['pass'] . ' "./tmp/' . $file_name . '" 2>&1');
+	 		if(empty($out)) {
+		 		$tmp = "key" . $_SESSION['idx'];
+		 		$_SESSION['dataUpload'][$tmp] = "<tr><td>" . $file_name . "</td><td style='text-align: center;'><form action=\"app.php\" method=\"post\" enctype=\"multipart/form-data\"><input type=\"hidden\" value=\"" . $file_name . ".sha256\" name=\"hapus\"/><input type=\"hidden\" value=\"" . $_SESSION['idx'] . "\" name=\"idxHapus\"/><input type=\"Submit\" value=\"Delete\" name=\"submit\"/></form></td>";
+		 		$_SESSION['idx'] += 1;
+		 		$_SESSION['valid'] = 'Sign Success!';
+		 	} else {
+		 		$_SESSION['valid'] = 'There is an error while signing';
+		 	}
+	 	}
+	 	else if($_POST['submit'] == "verify") {
+	 		if($multiple) {
+		 		exec('openssl x509 -in "./tmp/' . $cert_name . '" -pubkey -noout > "./tmp/' . $file_name . '.pub"');
+	 			$out = shell_exec('openssl dgst -sha256 -verify "./tmp/' . $file_name . '.pub" -signature "./signature/' . $file_name . '.sha256" "./tmp/' . $file_name . '"');
+		 		$_SESSION['valid'] = $out;
+		 	}
+		 	else {
+		 		$_SESSION['valid'] = "File not signed";
+		 	}
+	 	}
+	} else {
+		$_SESSION['valid'] = "Unsupported certificate format";
+	}
 
 	unlink("./tmp/" . $file_name . ".pub");
  	unlink("./tmp/" . $cert_name);
--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             text-align: center;
         }
         td {
-            border: 1px solid #ddd !important;            
+            border: 1px solid #ddd !important;           
         }
     </style>
 </head>
@@ -49,7 +49,7 @@
                         <input type="submit" value="sign" name="submit" class="btn btn-default">
                     </div>
                     <div class="col-sm-offset-2 col-sm-3">
-                        <input type="submit" value="verify" name="submit" class="btn btn-default">
+                        <input type="submit" value="verify" name="submit" class="btn btn-default" style="float:right;">
                     </div>
                 </div>
             </form>
@@ -60,21 +60,25 @@
                 echo $_SESSION['valid'];
                 unset($_SESSION['valid']);
             }
-            if(!isset($_SESSION['dataUpload'])) {
-                $_SESSION['dataUpload'] = array();
-            }
             if(!isset($_SESSION['idx'])) {
                  $_SESSION['idx'] = 0;
-            }   
+            }
+            if(!isset($_SESSION['dataUpload'])) {
+                $_SESSION['dataUpload'] = array();
+                foreach (glob('./signature/*.sha256') as $fileName) {
+                    $fileName = substr($fileName,12);
+                    $tmp = "key" . $_SESSION['idx'];
+                    $_SESSION['dataUpload'][$tmp] = "<tr><td>" . $fileName . "</td><td style='text-align: center;'><form action=\"app.php\" method=\"post\" enctype=\"multipart/form-data\"><input type=\"hidden\" value=\"" . $fileName . "\" name=\"hapus\"/><input type=\"hidden\" value=\"" . $_SESSION['idx'] . "\" name=\"idxHapus\"/><input type=\"Submit\" value=\"Delete\" name=\"submit\"/></form></td>";
+                    $_SESSION['idx'] += 1;
+                }
+            }
         ?>
         <div class="result">
             <table class="table table-hover">
                 <thead>
                     <tr>
                         <th>File Name</th>
-                        <th width="20%">Upload Time</th>
-                        <th width="7%">Delete</th>
-                        <th th width="20%">File Size</th>
+                        <th width="30%">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
